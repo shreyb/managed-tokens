b866b7569752acd7b1ef7ffb2100f9ec73d3ae9e
Shreyas Bhat <sbhat@fnal.gov>
2022-08-13 21:16:05 +0000

    Added descriptions to test cases

--- worker/channelsForWorkers_test.go
+++ worker/channelsForWorkers_test.go
@@ -12,29 +12,32 @@ import (
 
 func TestNewChannelsForWorkersDefaultBuffer(t *testing.T) {
 	type testCase struct {
+		description         string
 		userInputBufferSize int
 		expectedBufferSize  int
 	}
 	randomBufferSize := rand.Intn(100)
 
 	testCases := []testCase{
-		{0, 1},
-		{1, 1},
-		{randomBufferSize, randomBufferSize},
+		{"User input 0 buffer - should override and set buffer to 1", 0, 1},
+		{"User input 1 buffer - should set buffer to 1", 1, 1},
+		{"Random buffer size - should be preserved", randomBufferSize, randomBufferSize},
 	}
 
 	for _, test := range testCases {
-		n := NewChannelsForWorkers(test.userInputBufferSize)
-		if _, ok := n.(*channelGroup); !ok {
-			t.Errorf("Got wrong return type for NewChannelsForWorkers.  Wanted %T, got %T", &channelGroup{}, n)
-		}
+		t.Run(test.description, func(t *testing.T) {
+			n := NewChannelsForWorkers(test.userInputBufferSize)
+			if _, ok := n.(*channelGroup); !ok {
+				t.Errorf("Got wrong return type for NewChannelsForWorkers.  Wanted %T, got %T", &channelGroup{}, n)
+			}
 
-		if size := cap(n.GetServiceConfigChan()); size != test.expectedBufferSize {
-			t.Errorf("Buffer size for GetServiceConfigChan() should be %d.  Got %d instead", test.expectedBufferSize, size)
-		}
+			if size := cap(n.GetServiceConfigChan()); size != test.expectedBufferSize {
+				t.Errorf("Buffer size for GetServiceConfigChan() should be %d.  Got %d instead", test.expectedBufferSize, size)
+			}
 
-		if size := cap(n.GetSuccessChan()); size != test.expectedBufferSize {
-			t.Errorf("Buffer size for GetSuccessChan() should be %d.  Got %d instead", test.expectedBufferSize, size)
-		}
+			if size := cap(n.GetSuccessChan()); size != test.expectedBufferSize {
+				t.Errorf("Buffer size for GetSuccessChan() should be %d.  Got %d instead", test.expectedBufferSize, size)
+			}
+		})
 	}
 }
