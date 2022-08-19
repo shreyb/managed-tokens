4dd96005baee659b50bd9d0619e735b6a73269c5
Shreyas Bhat <sbhat@fnal.gov>
2022-08-19 16:15:38 +0000

    Added admin_test unit tests.  Still issue with TestPrepareAdminErrorsForMessage with ordering of notifications.  It sometimes passes, sometimes doesn't

--- notifications/admin_test.go
+++ notifications/admin_test.go
@@ -1,8 +1,13 @@
 package notifications
 
-import "testing"
+import (
+	"reflect"
+	"sync"
+	"testing"
+)
 
 func TestAddSetupErrorToAdminErrors(t *testing.T) {
+	adminErrors = sync.Map{}
 	type testCase struct {
 		description     string
 		service         string
@@ -65,6 +70,7 @@ func TestAddSetupErrorToAdminErrors(t *testing.T) {
 }
 
 func TestAddPushErrorToAdminErrors(t *testing.T) {
+	adminErrors = sync.Map{}
 	type testCase struct {
 		description     string
 		service         string
@@ -134,6 +140,177 @@ func TestAddPushErrorToAdminErrors(t *testing.T) {
 							pushErrorsDatum,
 						)
 					}
+				}
+			},
+		)
+	}
+}
+
+func TestPrepareAdminErrorsForMessage(t *testing.T) {
+	adminErrors = sync.Map{}
+	finalCheckData := make(map[string]AdminDataFinal)
+	notifications := []Notification{
+		&setupError{
+			message: "Setup error 1",
+			service: "test_service1",
+		},
+		&setupError{
+			message: "Setup error 2",
+			service: "test_service1",
+		},
+		&setupError{
+			message: "Setup error 1",
+			service: "test_service2",
+		},
+		&pushError{
+			message: "Push error 1",
+			node:    "node1",
+			service: "test_service1",
+		},
+		&pushError{
+			message: "Push error 1",
+			node:    "node2",
+			service: "test_service1",
+		},
+		&pushError{
+			message: "Push error 1",
+			node:    "node3",
+			service: "test_service2",
+		},
+	}
+
+	pushErrorsMap := make(map[string]map[string]string)
+	// Prepare final test data
+	for _, n := range notifications {
+
+		data, ok := finalCheckData[n.GetService()]
+		if !ok {
+			finalCheckData[n.GetService()] = AdminDataFinal{}
+		}
+
+		switch notificationValue := n.(type) {
+		case *setupError:
+			data.SetupErrors = append(data.SetupErrors, notificationValue.message)
+			finalCheckData[notificationValue.service] = data
+		case *pushError:
+			// Populate pushErrorsMap for processing below
+			_, ok := pushErrorsMap[notificationValue.service]
+			if !ok {
+				pushErrorsMap[notificationValue.service] = make(map[string]string)
+			}
+			pushErrorsMap[notificationValue.service][notificationValue.node] = notificationValue.message
+		}
+	}
+
+	for service, data := range finalCheckData {
+		data.PushErrorsTable = PrepareTableStringFromMap(
+			pushErrorsMap[service],
+			"The following is a list of nodes on which all vault tokens were not refreshed, and the corresponding roles for those failed token refreshes:",
+			[]string{"Node", "Error"},
+		)
+		finalCheckData[service] = data
+	}
+
+	// Populate test data into adminErrors
+	for _, n := range notifications {
+		addErrorToAdminErrors(n)
+	}
+
+	finalTestData := prepareAdminErrorsForMessage()
+
+	if !reflect.DeepEqual(finalTestData, finalCheckData) {
+		t.Errorf(
+			"Final test data did not match expected data.  Expected %v, got %v",
+			finalCheckData,
+			finalTestData,
+		)
+	}
+
+}
+
+func TestIsEmpty(t *testing.T) {
+	type testCase struct {
+		description    string
+		loaderFunc     func() adminData
+		expectedResult bool
+	}
+
+	testCases := []testCase{
+		{
+			"Empty adminData",
+			func() adminData {
+				return adminData{}
+			},
+			true,
+		},
+		{
+			"adminData has empty non-nil setupErrors",
+			func() adminData {
+				return adminData{
+					SetupErrors: []string{},
+				}
+			},
+			true,
+		},
+		{
+			"adminData has populated SetupErrors",
+			func() adminData {
+				return adminData{
+					SetupErrors: []string{"Test error"},
+				}
+			},
+			false,
+		},
+		{
+			"adminData has nil PushErrors",
+			func() adminData {
+				var m sync.Map
+				return adminData{
+					PushErrors: m,
+				}
+			},
+			true,
+		},
+		{
+			"adminData has populated PushErrors",
+			func() adminData {
+				var m sync.Map
+				for i := 1; i <= 10; i++ {
+					m.Store(i, struct{}{})
+				}
+				return adminData{
+					PushErrors: m,
+				}
+			},
+			false,
+		},
+		{
+			"adminData has populated SetupErrors and PushErrors",
+			func() adminData {
+				var m sync.Map
+				for i := 1; i <= 10; i++ {
+					m.Store(i, struct{}{})
+				}
+				return adminData{
+					SetupErrors: []string{"This is an error"},
+					PushErrors:  m,
+				}
+			},
+			false,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(
+			test.description,
+			func(t *testing.T) {
+				testData := test.loaderFunc()
+				if result := testData.isEmpty(); result != test.expectedResult {
+					t.Errorf(
+						"Got wrong result for isEmpty.  Expected %t, got %t",
+						test.expectedResult,
+						result,
+					)
 				}
 			},
 		)
