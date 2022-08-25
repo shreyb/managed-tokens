7e1d776bc085f221a9e06b33eaa2c6afdb72a7a6
Shreyas Bhat <sbhat@fnal.gov>
2022-08-25 03:40:55 +0000

    Finished db unit tests

--- db/db_test.go
+++ db/db_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"fmt"
@@ -148,10 +149,171 @@ func TestCreateUidsTable(t *testing.T) {
 	}
 }
 
-// Checks needed:
-// InsertUidsIntoTableFromFERRY(make some fake data, insert it into dummy db, make sure it's properly there)
-// ConfirmUIDsInTable(Give it an empty Db, make sure we have nothing; then give it a good DB, and make sure we have something)
-// GetUIDsByUsername(Give us a good DB, retrieve by username)
+// TestInsertAndConfirmUIDsInTable checks that we can insert and retrieve
+// FERRY data into the FERRYUIDDatabase
+func TestInsertAndConfirmUIDsInTable(t *testing.T) {
+	type testCase struct {
+		description           string
+		fakeData              []FerryUIDDatum
+		expectedRetrievedData []FerryUIDDatum
+	}
+
+	testCases := []testCase{
+		{
+			"Use an empty database with no data stored",
+			[]FerryUIDDatum{},
+			[]FerryUIDDatum{},
+		},
+		{
+			"Use a database with fake data",
+			[]FerryUIDDatum{
+				&checkDatum{
+					username: "testuser",
+					uid:      12345,
+				},
+				&checkDatum{
+					username: "anothertestuser",
+					uid:      67890,
+				},
+			},
+			[]FerryUIDDatum{
+				&checkDatum{
+					username: "testuser",
+					uid:      12345,
+				},
+				&checkDatum{
+					username: "anothertestuser",
+					uid:      67890,
+				},
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(
+			test.description,
+			func(t *testing.T) {
+				goodDbLocation := path.Join("/tmp/", fmt.Sprintf("managed-tokens-test-%d.db", rand.Intn(10000)))
+				defer os.Remove(goodDbLocation)
+
+				goodTestDb, err := OpenOrCreateDatabase(goodDbLocation)
+				if err != nil {
+					t.Errorf("Could not create new database, %s", err)
+				}
+				defer goodTestDb.Close()
+
+				if err := goodTestDb.InsertUidsIntoTableFromFERRY(context.Background(), test.fakeData); err != nil {
+					t.Error("Could not insert fake data into test database")
+				}
+
+				retrievedData, err := goodTestDb.ConfirmUIDsInTable(context.Background())
+				if err != nil {
+					t.Error("Could not retrieve fake data from test database")
+				}
+
+				for _, expectedDatum := range test.expectedRetrievedData {
+					found := false
+					for _, testDatum := range retrievedData {
+						if (testDatum.Uid() == expectedDatum.Uid()) && (testDatum.Username() == expectedDatum.Username()) {
+							found = true
+							break
+						}
+					}
+					if !found {
+						t.Errorf(
+							"Did not find expected datum %v in retrieved data",
+							expectedDatum,
+						)
+					}
+				}
+			},
+		)
+	}
+
+}
+
+// TestGetUIDsByUsername checks that we can retrieve the proper UID given a particular username.
+// If we give a user that does not exist, we should get 0 as the result, and an error
+func TestGetUIDsByUsername(t *testing.T) {
+
+	type testCase struct {
+		description   string
+		testUsername  string
+		expectedUid   int
+		expectedError error
+	}
+
+	testCases := []testCase{
+		{
+			"Get UID for user we know is in the database",
+			"testuser",
+			12345,
+			nil,
+		},
+		{
+			"Attempt to get UID for user who is not in the database",
+			"nonexistentuser",
+			0,
+			errors.New("sql: no rows in result set"),
+		},
+	}
+
+	fakeData := []FerryUIDDatum{
+		&checkDatum{
+			username: "testuser",
+			uid:      12345,
+		},
+		&checkDatum{
+			username: "anothertestuser",
+			uid:      67890,
+		},
+	}
+
+	goodDbLocation := path.Join("/tmp/", fmt.Sprintf("managed-tokens-test-%d.db", rand.Intn(10000)))
+	defer os.Remove(goodDbLocation)
+
+	goodTestDb, err := OpenOrCreateDatabase(goodDbLocation)
+	if err != nil {
+		t.Errorf("Could not create new database, %s", err)
+	}
+	defer goodTestDb.Close()
+
+	if err := goodTestDb.InsertUidsIntoTableFromFERRY(context.Background(), fakeData); err != nil {
+		t.Error("Could not insert fake data into test database")
+	}
+
+	for _, test := range testCases {
+		t.Run(
+			test.description,
+			func(t *testing.T) {
+				ctx := context.Background()
+				retrievedUid, err := goodTestDb.GetUIDByUsername(ctx, test.testUsername)
+				if err != nil {
+					if test.expectedError != nil {
+						// if err.Error() != test.expectedError.Error() {
+						if err.Error() != test.expectedError.Error() {
+							t.Errorf(
+								"Got wrong error.  Expected %v, got %v",
+								test.expectedError,
+								err,
+							)
+						}
+					} else {
+						t.Error("Could not get UID by username")
+					}
+				}
+				if retrievedUid != test.expectedUid {
+					t.Errorf(
+						"Retrieved UID and expected UID do not match.  Expected %d, got %d",
+						test.expectedUid,
+						retrievedUid,
+					)
+				}
+			},
+		)
+	}
+
+}
 
 // checkSchema is a testing utility function to make sure that a test FERRYUIDDatabase has the right schema
 func checkSchema(f *FERRYUIDDatabase) error {
