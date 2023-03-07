shreyb/managed-tokens

commit 70eb44cda758b5076c60bb6ed8f61b23586cbc13

Fix a race condition where if we hit timeout and return from the notifications sorter func before all notifications are sent from the workers to the notification listeners, the senders block forever

cmd/token-push/notificationsManager.go
@@ -50,7 +50,13 @@ func startListenerOnWorkerNotificationChans(ctx context.Context, nChan chan noti
 	workerNotificationWg.Add(1)
 	go func() {
 		defer workerNotificationWg.Done()
-		for n := range nChan {
+		select {
+		case <-ctx.Done():
+			return
+		case n, chanOpen := <-nChan:
+			if !chanOpen {
+				return
+			}
 			notificationsFromWorkersChan <- n
 		}
 	}()

commit fd7b3b31ca6e5aa544b7b9137b4faa0a0cba51ba

Fixed race condition where we weren't reading all notifications from workers.  Bug introduced in 70eb44c.

cmd/token-push/notificationsManager.go
@@ -49,19 +49,18 @@ func registerServiceNotificationsChan(ctx context.Context, s service.Service, wg
 // instantiates a notifications.Notifications chan can register its own listener (this func), and all those notifications are
 // aggregated elsewhere
 func startListenerOnWorkerNotificationChans(ctx context.Context, nChan chan notifications.Notification) {
-	f := func() { directNotificationsToManagers(ctx) }
+	f := func() { directNotificationsToManagers(ctx) } // TODO (move to next line?)
 	go func() {
 		notificationSorter.Do(f)
 	}()
 	workerNotificationWg.Add(1)
 	go func() {
 		defer workerNotificationWg.Done()
-		select {
-		case <-ctx.Done():
-			return
-		case n, chanOpen := <-nChan:
-			if !chanOpen {
+		for n := range nChan {
+			select {
+			case <-ctx.Done():
 				return
+			default:
 			}
 			notificationsFromWorkersChan <- n
 		}