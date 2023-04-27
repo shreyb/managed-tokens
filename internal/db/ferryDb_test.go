21782983dfd7d079221b9d1cdfde497fda9fbbc5
Shreyas Bhat <sbhat@fnal.gov>
2023-04-27 02:59:06 +0000

    Cleaned up unit tests for FERRY-related DB functions

--- internal/db/ferryDb_test.go
+++ internal/db/ferryDb_test.go
@@ -2,43 +2,30 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"math/rand"
 	"os"
 	"path"
 	"testing"
-
-	_ "github.com/mattn/go-sqlite3"
+	// _ "github.com/mattn/go-sqlite3"
 )
 
-// TestInsertAndConfirmUIDsInTable checks that we can insert and retrieve
-// FERRY data into the FERRYUIDDatabase
-func TestInsertAndConfirmUIDsInTable(t *testing.T) {
+// TestInsertUidsIntoTableFromFERRY checks that we can insert FERRY data into the ManagedTokensDatabase correctly
+func TestInsertUidsIntoTableFromFERRY(t *testing.T) {
 	type testCase struct {
-		description           string
-		fakeData              []FerryUIDDatum
-		expectedRetrievedData []FerryUIDDatum
+		description string
+		fakeData    []FerryUIDDatum
 	}
 
 	testCases := []testCase{
 		{
 			"Use an empty database with no data stored",
 			[]FerryUIDDatum{},
-			[]FerryUIDDatum{},
 		},
 		{
 			"Use a database with fake data",
-			[]FerryUIDDatum{
-				&ferryUidDatum{
-					username: "testuser",
-					uid:      12345,
-				},
-				&ferryUidDatum{
-					username: "anothertestuser",
-					uid:      67890,
-				},
-			},
 			[]FerryUIDDatum{
 				&ferryUidDatum{
 					username: "testuser",
@@ -69,29 +56,85 @@ func TestInsertAndConfirmUIDsInTable(t *testing.T) {
 					t.Error("Could not insert fake data into test database")
 				}
 
-				retrievedData, err := goodTestDb.ConfirmUIDsInTable(context.Background())
+				retrievedData := make([]FerryUIDDatum, 0, len(test.fakeData))
+				rows, err := goodTestDb.db.Query("SELECT username, uid FROM uids")
 				if err != nil {
 					t.Error("Could not retrieve fake data from test database")
 				}
-
-				for _, expectedDatum := range test.expectedRetrievedData {
-					found := false
-					for _, testDatum := range retrievedData {
-						if (testDatum.Uid() == expectedDatum.Uid()) && (testDatum.Username() == expectedDatum.Username()) {
-							found = true
-							break
-						}
+				for rows.Next() {
+					var dataUsername string
+					var dataUid int
+					if err := rows.Scan(&dataUsername, &dataUid); err != nil {
+						t.Error("Could not store retrieved test database data locally")
 					}
-					if !found {
-						t.Errorf(
-							"Did not find expected datum %v in retrieved data.  Retrieved data: %v",
-							expectedDatum,
-							retrievedData,
-						)
-					}
+					retrievedData = append(retrievedData, &ferryUidDatum{dataUsername, dataUid})
+				}
+				if !slicesHaveSameElements(
+					ferryUIDDatumInterfaceSlicetoStructSlice(test.fakeData),
+					ferryUIDDatumInterfaceSlicetoStructSlice(retrievedData),
+				) {
+					t.Errorf("Expected data and retrieved data do not match.  Expected %v, got %v", test.fakeData, retrievedData)
 				}
 			},
 		)
+	}
+}
+
+// TestConfirmUIDsInTable checks that ConfirmUIDsInTable returns values from the ManagedTokensDatabase correctly
+func TestConfirmUIDsInTable(t *testing.T) {
+	type testCase struct {
+		description string
+		fakeData    []ferryUidDatum
+	}
+
+	testCases := []testCase{
+		{
+			"Use an empty database with no data stored",
+			[]ferryUidDatum{},
+		},
+		{
+			"Use a database with fake data",
+			[]ferryUidDatum{
+				{
+					username: "testuser",
+					uid:      12345,
+				},
+				{
+					username: "anothertestuser",
+					uid:      67890,
+				},
+			},
+		},
+	}
+
+	for _, test := range testCases {
+
+		goodDbLocation := path.Join("/tmp/", fmt.Sprintf("managed-tokens-test-%d.db", rand.Intn(10000)))
+		defer os.Remove(goodDbLocation)
+
+		goodTestDb, err := OpenOrCreateDatabase(goodDbLocation)
+		if err != nil {
+			t.Errorf("Could not create new database, %s", err)
+		}
+		defer goodTestDb.Close()
+
+		for _, datum := range test.fakeData {
+			if _, err := goodTestDb.db.Exec("INSERT into uids (username, uid) VALUES (?,?)", datum.username, datum.uid); err != nil {
+				t.Errorf("Could not insert test data into database")
+			}
+		}
+
+		retrievedData, err := goodTestDb.ConfirmUIDsInTable(context.Background())
+		if err != nil {
+			t.Error("Could not retrieve fake data from test database")
+		}
+
+		if !slicesHaveSameElements(
+			ferryUIDDatumInterfaceSlicetoStructSlice(retrievedData),
+			test.fakeData,
+		) {
+			t.Errorf("Expected data and retrieved data do not match.  Expected %v, got %v", test.fakeData, retrievedData)
+		}
 	}
 }
 
@@ -117,7 +160,7 @@ func TestGetUIDsByUsername(t *testing.T) {
 			"Attempt to get UID for user who is not in the database",
 			"nonexistentuser",
 			0,
-			errors.New("sql: no rows in result set"),
+			sql.ErrNoRows,
 		},
 	}
 
@@ -153,8 +196,7 @@ func TestGetUIDsByUsername(t *testing.T) {
 				retrievedUid, err := goodTestDb.GetUIDByUsername(ctx, test.testUsername)
 				if err != nil {
 					if test.expectedError != nil {
-						// if err.Error() != test.expectedError.Error() {
-						if err.Error() != test.expectedError.Error() {
+						if !errors.Is(err, test.expectedError) {
 							t.Errorf(
 								"Got wrong error.  Expected %v, got %v",
 								test.expectedError,
@@ -176,6 +218,21 @@ func TestGetUIDsByUsername(t *testing.T) {
 		)
 	}
 
+}
+
+// ferryUIDDatumInterfaceSlicetoStructSlice is a helper function that converts a slice of
+// FerryUIDDatum to a slice of the concrete type used in this package, ferryUidDatum
+func ferryUIDDatumInterfaceSlicetoStructSlice(f []FerryUIDDatum) []ferryUidDatum {
+	retVal := make([]ferryUidDatum, 0, len(f))
+	for _, elt := range f {
+		eltVal, ok := elt.(*ferryUidDatum)
+		if ok {
+			if eltVal != nil {
+				retVal = append(retVal, *eltVal)
+			}
+		}
+	}
+	return retVal
 }
 
 // TestOpenFERRYUIDDatabase checks that Open properly opens the underlying SQLite database underlying FERRYUIDDatabase, and returns the proper error
