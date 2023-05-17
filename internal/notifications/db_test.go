2be602c9230a305401e5e53bfd7ff97d26090878
Shreyas Bhat <sbhat@fnal.gov>
2023-05-17 21:40:46 +0000

    Added some test cases to TestAdjustErrorCountsByServiceAndDirectNotification

--- internal/notifications/db_test.go
+++ internal/notifications/db_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestAdjustErrorCountsByServiceAndDirectNotification(t *testing.T) {
 	type testCase struct {
+		helptext string
 		Notification
 		errorCounts         *serviceErrorCounts
 		notificationMinimum int
@@ -16,6 +17,7 @@ func TestAdjustErrorCountsByServiceAndDirectNotification(t *testing.T) {
 
 	testCases := []testCase{
 		{
+			helptext: "No pre-existing errors, get setupError",
 			Notification: &setupError{
 				"This is a setup error",
 				"service1",
@@ -31,16 +33,49 @@ func TestAdjustErrorCountsByServiceAndDirectNotification(t *testing.T) {
 				pushErrors:  map[string]int{},
 			},
 		},
+		{
+			helptext: "Pre-existing errors, get setupError, not enough for threshhold",
+			Notification: &setupError{
+				"This is a setup error",
+				"service1",
+			},
+			errorCounts: &serviceErrorCounts{
+				setupErrors: 1,
+				pushErrors:  map[string]int{},
+			},
+			notificationMinimum: 3,
+			expectedShouldSend:  false,
+			expectedErrorCounts: &serviceErrorCounts{
+				setupErrors: 2,
+				pushErrors:  map[string]int{},
+			},
+		},
+		{
+			helptext: "Pre-existing errors, get setupError, enough for threshhold",
+			Notification: &setupError{
+				"This is a setup error",
+				"service1",
+			},
+			errorCounts: &serviceErrorCounts{
+				setupErrors: 2,
+				pushErrors:  map[string]int{},
+			},
+			notificationMinimum: 3,
+			expectedShouldSend:  true,
+			expectedErrorCounts: &serviceErrorCounts{
+				setupErrors: 0,
+				pushErrors:  map[string]int{},
+			},
+		},
 	}
 
 	for _, test := range testCases {
 		result := adjustErrorCountsByServiceAndDirectNotification(test.Notification, test.errorCounts, test.notificationMinimum)
 		if result != test.expectedShouldSend {
-			t.Errorf("Got wrong decision on whether/not to send notification. Expected %t, got %t", test.expectedShouldSend, result)
+			t.Errorf("Got wrong decision on whether/not to send notification for test %s. Expected %t, got %t", test.helptext, test.expectedShouldSend, result)
 		}
-		// if test.expectedErrorCounts.setupErrors != test.errorCounts.setupErrors {
 		if !reflect.DeepEqual(test.expectedErrorCounts, test.errorCounts) {
-			t.Errorf("Got wrong serviceErrorCounts.  Expected %v, got %v", test.expectedErrorCounts, test.errorCounts)
+			t.Errorf("Got wrong serviceErrorCounts for test %s.  Expected %v, got %v", test.helptext, test.expectedErrorCounts, test.errorCounts)
 		}
 	}
 
