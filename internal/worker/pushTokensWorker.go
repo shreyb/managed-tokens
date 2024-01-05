9bc093d0ca19c1c8592faa0485bff8489994bf98
Shreyas Bhat <sbhat@fnal.gov>
2024-01-05 14:37:10 -0600

    Added copyright to top of every source file

--- internal/worker/pushTokensWorker.go
+++ internal/worker/pushTokensWorker.go
@@ -1,3 +1,18 @@
+// COPYRIGHT 2024 FERMI NATIONAL ACCELERATOR LABORATORY
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package worker
 
 import (
@@ -350,6 +365,7 @@ func parseDefaultRoleFileDestinationTemplateFromConfig(c *Config) (string, error
 	}
 	return b.String(), nil
 }
+
 // prepareDefaultRoleFile prepares the role file for the given service config
 func prepareDefaultRoleFile(sc *Config) (string, error) {
 	serviceLogger := log.WithFields(log.Fields{
