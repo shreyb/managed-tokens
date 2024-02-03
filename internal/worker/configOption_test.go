1698e5a86a838ea2a01c9532fb71b13d224dd9b2
Shreyas Bhat <sbhat@fnal.gov>
2024-02-02 23:50:29 -0600

    Consolidated tests into a single table-driven test

--- internal/worker/configOption_test.go
+++ internal/worker/configOption_test.go
@@ -22,76 +22,107 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSetCommandEnvironment(t *testing.T) {
+func TestAllConfigOptions(t *testing.T) {
 	c := new(Config)
-	cmdEnvFunc := func(e *environment.CommandEnvironment) { e.SetCondorCreddHost("creddHost") }
-	funcOpt := SetCommandEnvironment(cmdEnvFunc)
-	funcOpt(c)
-	assert.Equal(t, "creddHost", c.CommandEnvironment.GetValue(environment.CondorCreddHost))
-}
+	type testCase struct {
+		description   string
+		funcOptSetup  func() ConfigOption // Get us our test ConfigOption to apply to c
+		expectedValue any
+		testValueFunc func() any // The return value of this func is the value we're testing
+	}
 
-// // SetUserPrincipal returns a func(*Config) with the UserPrincipal field set to the passed in value
-func TestSetUserPrincipal(t *testing.T) {
-	c := new(Config)
-	userPrincipal := "myuserPRINCIPAL"
-	funcOpt := SetUserPrincipal(userPrincipal)
-	funcOpt(c)
-	assert.Equal(t, userPrincipal, c.UserPrincipal)
-}
+	testCases := []testCase{
+		{
+			"SetCommandEnvironment",
+			func() ConfigOption {
+				cmdEnvFunc := func(e *environment.CommandEnvironment) { e.SetCondorCreddHost("creddHost") }
+				return SetCommandEnvironment(cmdEnvFunc)
+			},
+			"creddHost",
+			func() any { return c.CommandEnvironment.GetValue(environment.CondorCreddHost) },
+		},
+		{
+			"SetUserPrincipal",
+			func() ConfigOption {
+				userPrincipal := "myuserPRINCIPAL"
+				return SetUserPrincipal(userPrincipal)
+			},
+			"myuserPRINCIPAL",
+			func() any { return c.UserPrincipal },
+		},
+		{
+			"SetNodes",
+			func() ConfigOption {
+				nodes := []string{"node1", "node2"}
+				return SetNodes(nodes)
+			},
+			[]string{"node1", "node2"},
+			func() any { return c.Nodes },
+		},
+		{
+			"TestSetAccount",
+			func() ConfigOption {
+				account := "myaccount"
+				return SetAccount(account)
+			},
+			"myaccount",
+			func() any { return c.Account },
+		},
+		{
+			"TestSetKeytabPath",
+			func() ConfigOption {
+				keytabPath := "/path/to/keytab"
+				return SetKeytabPath(keytabPath)
+			},
+			"/path/to/keytab",
+			func() any { return c.KeytabPath },
+		},
+		{
+			"TestSetDesiredUID",
+			func() ConfigOption {
+				uid := uint32(42)
+				return SetDesiredUID(uid)
+			},
+			uint32(42),
+			func() any { return c.DesiredUID },
+		},
+		{
+			"TestSetSchedds",
+			func() ConfigOption {
+				schedds := []string{"schedd1", "schedd2"}
+				return SetSchedds(schedds)
+			},
+			[]string{"schedd1", "schedd2"},
+			func() any { return c.Schedds },
+		},
+		{
+			"TestSetVaultServer",
+			func() ConfigOption {
+				vaultServer := "vault.server.host"
+				return SetVaultServer(vaultServer)
+			},
+			"vault.server.host",
+			func() any { return c.VaultServer },
+		},
+		{
+			"TestSetServiceCreddVaultTokenPathRoot",
+			func() ConfigOption {
+				root := "/path/to/vault/token/root"
+				return SetServiceCreddVaultTokenPathRoot(root)
+			},
+			"/path/to/vault/token/root",
+			func() any { return c.ServiceCreddVaultTokenPathRoot },
+		},
+	}
 
-func TestSetNodes(t *testing.T) {
-	c := new(Config)
-	nodes := []string{"node1", "node2"}
-	funcOpt := SetNodes(nodes)
-	funcOpt(c)
-	assert.Equal(t, nodes, c.Nodes)
-}
-
-func TestSetAccount(t *testing.T) {
-	c := new(Config)
-	account := "myaccount"
-	funcOpt := SetAccount(account)
-	funcOpt(c)
-	assert.Equal(t, account, c.Account)
-}
-
-func TestSetKeytabPath(t *testing.T) {
-	c := new(Config)
-	keytabPath := "/path/to/keytab"
-	funcOpt := SetKeytabPath(keytabPath)
-	funcOpt(c)
-	assert.Equal(t, keytabPath, c.KeytabPath)
-
-}
-
-func TestSetDesiredUID(t *testing.T) {
-	c := new(Config)
-	uid := uint32(42)
-	funcOpt := SetDesiredUID(uid)
-	funcOpt(c)
-	assert.Equal(t, uid, c.DesiredUID)
-}
-
-func TestSetSchedds(t *testing.T) {
-	c := new(Config)
-	schedds := []string{"schedd1", "schedd2"}
-	funcOpt := SetSchedds(schedds)
-	funcOpt(c)
-	assert.Equal(t, schedds, c.Schedds)
-}
-
-func TestSetVaultServer(t *testing.T) {
-	c := new(Config)
-	vaultServer := "vault.server.host"
-	funcOpt := SetVaultServer(vaultServer)
-	funcOpt(c)
-	assert.Equal(t, vaultServer, c.VaultServer)
-}
-
-func TestSetServiceCreddVaultTokenPathRoot(t *testing.T) {
-	c := new(Config)
-	root := "/path/to/vault/token/root"
-	funcOpt := SetServiceCreddVaultTokenPathRoot(root)
-	funcOpt(c)
-	assert.Equal(t, root, c.ServiceCreddVaultTokenPathRoot)
+	for _, test := range testCases {
+		t.Run(
+			test.description,
+			func(t *testing.T) {
+				funcOpt := test.funcOptSetup()
+				funcOpt(c)
+				assert.Equal(t, test.expectedValue, test.testValueFunc())
+			},
+		)
+	}
 }
