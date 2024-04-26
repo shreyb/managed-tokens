945c9a280b113f4998a4221aaa5c16dd96511623
Shreyas Bhat <sbhat@fnal.gov>
2024-04-26 10:33:08 -0500

    Moved onboardingUsage() into main.go

--- cmd/run-onboarding-managed-tokens/main.go
+++ cmd/run-onboarding-managed-tokens/main.go
@@ -164,6 +164,13 @@ func initServices() error {
 	return nil
 }
 
+// Custom usage function for positional argument.
+func onboardingUsage() {
+	fmt.Printf("Usage: %s [OPTIONS] service...\n", os.Args[0])
+	fmt.Printf("service must be of the form 'experiment_role', e.g. 'dune_production'\n")
+	pflag.PrintDefaults()
+}
+
 func initLogs() {
 	// Set up logs
 	log.SetLevel(log.DebugLevel)
