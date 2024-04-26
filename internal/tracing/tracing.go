47cd3e53a2392d9d7928fdc258c0b8bb5ba45e76
Shreyas Bhat <sbhat@fnal.gov>
2024-04-26 11:38:54 -0500

    Added ability to pass key-value pairs to log/tracing funcs

--- internal/tracing/tracing.go
+++ internal/tracing/tracing.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	log "github.com/sirupsen/logrus"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/sdk/resource"
@@ -32,9 +33,20 @@ func JaegerTraceProvider(url string) (*sdktrace.TracerProvider, func(context.Con
 	return tp, func(ctx context.Context) { tp.Shutdown(ctx) }, nil
 }
 
+// KeyValueForLog is a struct that holds a key and value for logging purposes
+type KeyValueForLog struct {
+	Key   string
+	Value string
+}
+
 // LogErrorWithTrace logs an error message modifies the passed in trace span.
 // It sets the status of the span to an error, records the error, and logs the error message using the provided logger.
-func LogErrorWithTrace(span trace.Span, logger *log.Entry, msg string) {
+func LogErrorWithTrace(span trace.Span, logger *log.Entry, msg string, keyValues ...KeyValueForLog) {
+	for _, keyvalue := range keyValues {
+		span.SetAttributes(attribute.KeyValue{Key: attribute.Key(keyvalue.Key), Value: attribute.StringValue(keyvalue.Value)})
+		logger = logger.WithField(keyvalue.Key, keyvalue.Value)
+	}
+
 	err := errors.New(msg)
 	span.SetStatus(codes.Error, msg)
 	span.RecordError(err)
@@ -42,7 +54,12 @@ func LogErrorWithTrace(span trace.Span, logger *log.Entry, msg string) {
 }
 
 // LogSuccessWithTrace logs a success message with the passed in logger and sets the passed-in span's status to OK
-func LogSuccessWithTrace(span trace.Span, logger *log.Entry, msg string) {
+func LogSuccessWithTrace(span trace.Span, logger *log.Entry, msg string, keyValues ...KeyValueForLog) {
+	for _, keyvalue := range keyValues {
+		span.SetAttributes(attribute.KeyValue{Key: attribute.Key(keyvalue.Key), Value: attribute.StringValue(keyvalue.Value)})
+		logger = logger.WithField(keyvalue.Key, keyvalue.Value)
+	}
+
 	span.SetStatus(codes.Ok, msg)
 	logger.Info(msg)
 }
