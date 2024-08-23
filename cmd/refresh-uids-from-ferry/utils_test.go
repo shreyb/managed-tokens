0ca87df97ddbd0e89216e3088759cf3a3edb1e1f
Shreyas Bhat <sbhat@fnal.gov>
2024-08-23 11:05:40 -0500

    Change getAndAggregateFERRYData so it returns an error.  This enabled me to write a test for that func

--- cmd/refresh-uids-from-ferry/utils_test.go
+++ cmd/refresh-uids-from-ferry/utils_test.go
@@ -16,6 +16,10 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 	"testing"
@@ -24,6 +28,7 @@ import (
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/fermitools/managed-tokens/internal/db"
 	"github.com/fermitools/managed-tokens/internal/notifications"
 	"github.com/fermitools/managed-tokens/internal/testUtils"
 )
@@ -140,4 +145,86 @@ func TestSendSetupErrorToAdminMgr(t *testing.T) {
 	assert.Eventually(t, func() bool {
 		return <-done
 	}, time.Second, 10*time.Millisecond)
+}
+
+func TestGetAndAggregateFERRYData(t *testing.T) {
+	nonNilErr := errors.New("this is a test error")
+
+	type testCase struct {
+		description       string
+		username          string
+		authFunc          func() func(context.Context, string, string) (*http.Response, error)
+		ferryDataChan     chan db.FerryUIDDatum
+		notificationsChan chan notifications.SourceNotification
+		expectedErr       error
+	}
+
+	testCases := []testCase{
+		{
+			"error getting ferry data",
+			"test_user",
+			func() func(context.Context, string, string) (*http.Response, error) {
+				return func(ctx context.Context, url, verb string) (*http.Response, error) {
+					return nil, errors.New("this is a test error")
+				}
+			},
+			// buffer these channels so we can send values without preemptively starting up listeners like we normally would
+			make(chan db.FerryUIDDatum, 1),
+			make(chan notifications.SourceNotification, 1),
+			nonNilErr,
+		},
+		{
+			"no error getting ferry data",
+			"test_user",
+			func() func(context.Context, string, string) (*http.Response, error) {
+				return func(ctx context.Context, url, verb string) (*http.Response, error) {
+					fakeResponseBody := `{"ferry_status":"success","ferry_error":[],"ferry_output":{"expirationdate":"2024-01-01T00:00:00Z","fullname":"Test User","groupaccount":false,"status":true,"uid":12345,"vopersonid":"12345"}}`
+					return &http.Response{
+						StatusCode: 200,
+						Body:       io.NopCloser(strings.NewReader(fakeResponseBody)),
+					}, nil
+				}
+			},
+			// buffer these channels so we can send values without preemptively starting up listeners like we normally would
+			make(chan db.FerryUIDDatum, 1),
+			make(chan notifications.SourceNotification, 1),
+			nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.username, func(t *testing.T) {
+			defer close(tc.ferryDataChan)
+			defer close(tc.notificationsChan)
+			err := getAndAggregateFERRYData(context.Background(), tc.username, tc.authFunc, tc.ferryDataChan, tc.notificationsChan)
+			assert.Equal(t, tc.expectedErr, err)
+
+			if tc.expectedErr != nil {
+				// If there is no expected error, we check that a notification got sent.  We don't care in this case about the contents
+				// of the notification, just that one was sent
+				testCtx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+				defer cancel()
+
+				select {
+				case <-testCtx.Done():
+					t.Error("Expected to receive a notification, but none received")
+				case <-tc.notificationsChan:
+					// This is the expected behavior, so we do nothing and let the test continue
+				}
+				return
+			}
+
+			// At this point, we don't expect an error, so the entry should have gotten sent on the ferryDataChan. Like before, we don't
+			// care about the contents of the entry, just that one was sent
+			testCtx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+
+			select {
+			case <-testCtx.Done():
+				t.Error("Expected to receive a notification, but none received")
+			case <-tc.ferryDataChan:
+				// This is the expected behavior
+			}
+		})
+	}
 }
